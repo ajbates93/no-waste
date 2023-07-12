7b63636356015203946a2fafb568d8010c4304ea
Alex Bates <ajbates93@gmail.com>
2023-07-12 22:05:05 +0100

    feat: add graphic to homepage

--- components/Header.vue
+++ components/Header.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-  <header class="p-5 dark:text-white dark:text-white flex items-center justify-end text-lg gap-5 font-semibold">
-    <NuxtLink to="/" class="mr-auto">AJB</NuxtLink>
-    <NuxtLink to="/blog">Blog</NuxtLink>
-    <NuxtLink to="/work">Work</NuxtLink>
+  <header class="p-5 dark:text-white flex items-center justify-end text-lg gap-5 font-semibold">
+    <NuxtLink to="/" class="mr-auto dark:text-custGreen font-extrabold text-2xl">NWH</NuxtLink>
+    <NuxtLink to="/recipes" class="dark:text-custGreen font-bold text-2xl uppercase">Find Recipes</NuxtLink>
+    <NuxtLink to="/mission" class="dark:text-custGreen font-bold text-2xl uppercase">About</NuxtLink>
     <a href="https://github.com/ajbates93" target="_blank">
-      <Icon name="iconoir-github" size="1.5rem" />
+      <Icon name="iconoir-sun-light" size="1.5rem" />
     </a>
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -25,12 +25,19 @@ const highlights: HomepageHighlightCard[] = [
 
 <template>
   <div class="max-w-screen-xl mx-auto">
-    <div class="max-w-screen-sm">
-      <h1 class="mt-10 mb-10 text-7xl text-custGreen font-bold text-left"><span class="text-gray-200 mr-3">No</span><span class="text-custGreen mr-3">Waste</span><span class="text-white mr-3">Here!</span></h1>
-      <h3 class="my-10 text-3xl text-gray-400 font-bold text-left">List your remaining ingredients in the fridge and find recipes that match exactly what you have.</h3>
-      <div class="flex my-10 gap-5">
-        <nuxt-link class="bg-gray-200 text-custNavy py-3 px-6 rounded-3xl font-bold" to="/recipes">Find Recipes</nuxt-link>
-        <nuxt-link class="bg-custNavyLight border border-custGreen text-gray-200 py-3 px-6 rounded-3xl font-bold" to="/get-involved">Get Involved</nuxt-link>
+    <div class="flex justify-between items-start">
+      <div class="w-4/6 mr-10">
+        <h1 class="mt-10 mb-10 text-7xl text-custGreen font-bold text-left"><span class="text-gray-200 mr-3">No</span><span class="text-custGreen mr-3">Waste</span><span class="text-white mr-3">Here!</span></h1>
+        <h3 class="my-10 text-3xl text-gray-400 font-bold text-left">List the remaining ingredients in your fridge and find recipes that match exactly what you've got.</h3>
+        <div class="flex my-10 gap-5">
+          <nuxt-link class="bg-gray-200 text-custNavy py-3 px-6 rounded-3xl font-bold" to="/recipes">Find Recipes</nuxt-link>
+          <nuxt-link class="bg-custNavyLight border border-custGreen text-gray-200 py-3 px-6 rounded-3xl font-bold" to="/get-involved">Get Involved</nuxt-link>
+        </div>
+      </div>
+      <div class="w-2/6 mb-10">
+        <picture>
+          <img src="/fruitveg.svg" class="w-full" />
+        </picture>
       </div>
     </div>
     <div class="grid grid-cols-3 gap-5">
